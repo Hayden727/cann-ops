8a84982b59fef0b08ee26c62a2e8b1fbe4617088
Nicet <1849404617@qq.com>
2025-01-07 19:50:08 +0800

    fix test script

--- src/math/sqrt/tests/st/test_sqrt.py
+++ src/math/sqrt/tests/st/test_sqrt.py
@@ -1,18 +1,9 @@
 import tensorflow as tf
 import numpy as np
 
-def sqrt_test(x):
-    """Compute the Swish activation function."""
-    tensor = tf.convert_to_tensor(x)
-    ori_dtype = tensor.dtype
-    compute_dtype = tf.float32
-    tensor = tf.cast(tensor, compute_dtype)
-    result = tf.cast(tf.math.sqrt(tensor), ori_dtype)
-    return result.numpy()
-
 def calc_expect_func(x, y):
     """
     calc_expect_func
     """
-    res = sqrt_test(x["value"])
+    res = np.sqrt(x['value'])
     return [res]
