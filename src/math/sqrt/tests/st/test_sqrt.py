e00219a924e4ef5a6c0219a0fd846c905743535c
Nicet <1849404617@qq.com>
2025-01-07 19:53:47 +0800

    fix test script

--- src/math/sqrt/tests/st/test_sqrt.py
+++ src/math/sqrt/tests/st/test_sqrt.py
@@ -1,9 +1,16 @@
 import tensorflow as tf
 import numpy as np
 
+def sqrt_test(x):
+    """Compute the Swish activation function."""
+    tensor = tf.convert_to_tensor(x)
+    sqrt_tensor = tf.math.sqrt(tensor)
+    re = sqrt_tensor.numpy()
+    return re
+
 def calc_expect_func(x, y):
     """
     calc_expect_func
     """
-    res = np.sqrt(x['value'])
+    res = sqrt_test(x['value'])
     return [res]
