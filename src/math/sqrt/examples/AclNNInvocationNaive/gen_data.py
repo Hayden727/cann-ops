bb9ab50d2ec9f8a4a78beeb7ff4c71e27e70f942
Nicet <1849404617@qq.com>
2025-01-07 21:06:31 +0800

    fix code check

--- src/math/sqrt/examples/AclNNInvocationNaive/gen_data.py
+++ src/math/sqrt/examples/AclNNInvocationNaive/gen_data.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 # -*- coding:utf-8 -*-
 # Copyright 2025 Huawei Technologies Co., Ltd
-import numpy as np
 import os
+import numpy as np
 
 def gen_golden_data_simple():
     dtype = np.float16
     output_shape = [1024, 1024]
+
     input_x = np.random.uniform(1, 4, output_shape).astype(dtype)
 
     golden = np.sqrt(input_x).astype(dtype)
+
     os.system("mkdir -p input")
     os.system("mkdir -p output")
     input_x.tofile("./input/input_x.bin")
--- src/math/sqrt/examples/AclNNInvocationNaive/verify_result.py
+++ src/math/sqrt/examples/AclNNInvocationNaive/verify_result.py
@@ -1,9 +1,13 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+# Copyright 2025 Huawei Technologies Co., Ltd
 import os
 import sys
+import logging
 import numpy as np
 
-loss = 1e-3 # 容忍偏差，一般fp16要求绝对误差和相对误差均不超过千分之一
-minimum = 10e-10
+LOSS = 1e-3 # 容忍偏差，一般fp16要求绝对误差和相对误差均不超过千分之一
+MINIMUM = 10e-10
 
 def verify_result(real_result, golden):
     dtype = np.float16
@@ -11,14 +15,14 @@ def verify_result(real_result, golden):
     golden = np.fromfile(golden, dtype=dtype) # 从bin文件读取预期运算结果
     result = np.abs(real_result - golden) # 计算运算结果和预期结果偏差
     deno = np.maximum(np.abs(real_result), np.abs(golden))  # 获取最大值并组成新数组
-    result_atol = np.less_equal(result, loss) # 计算绝对误差
-    result_rtol = np.less_equal(result / np.add(deno, minimum), loss) # 计算相对误差
+    result_atol = np.less_equal(result, LOSS) # 计算绝对误差
+    result_rtol = np.less_equal(result / np.add(deno, MINIMUM), LOSS) # 计算相对误差
     if not result_rtol.all() and not result_atol.all():
-        if np.sum(result_rtol == False) > real_result.size * loss and np.sum(result_atol == False) > real_result.size * loss: # 误差超出预期时返回打印错误，返回对比失败
-            print("[ERROR] result error")
+        if np.sum(result_rtol == False) > real_result.size * LOSS and np.sum(result_atol == False) > real_result.size * LOSS: # 误差超出预期时返回打印错误，返回对比失败
+            logging.error("[ERROR] result error")
             return False
-    print("test pass")
+    logging.info("test pass")
     return True
 
 if __name__ == '__main__':
-    verify_result(sys.argv[1],sys.argv[2])
+    verify_result(sys.argv[1], sys.argv[2])
