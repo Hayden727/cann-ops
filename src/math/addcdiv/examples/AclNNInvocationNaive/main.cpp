71144574bbfc5efe9e23fded04f14a17cd2cf0c1
Bellyboom <chen.x@mail.nwpu.edu.cn>
2025-01-09 14:04:41 +0000

    update src/math/addcdiv/examples/AclNNInvocationNaive/main.cpp. 版权
    Signed-off-by: Bellyboom <chen.x@mail.nwpu.edu.cn>

--- src/math/addcdiv/examples/AclNNInvocationNaive/main.cpp
+++ src/math/addcdiv/examples/AclNNInvocationNaive/main.cpp
@@ -1,11 +1,12 @@
-/**
- * @file main.cpp
- *
- * Copyright (C) 2024. Huawei Technologies Co., Ltd. All rights reserved.
- *
+/* 
+ * Copyright (C) Huawei Technologies Co., Ltd. 2025. All rights reserved.
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+/**
+ * @file main.cpp
  */
 #include <algorithm>
 #include <cstdint>
