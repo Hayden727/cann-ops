40a1a9986fb2fc19cf09572cc1af71300a5803e4
Yxymay <yinxinyue@mail.nwpu.edu.cn>
2025-01-10 14:25:50 +0000

    clean code

--- src/math/swish/tests/st/test_swish.py
+++ src/math/swish/tests/st/test_swish.py
@@ -4,10 +4,12 @@
 import tensorflow as tf
 import numpy as np
 
+
 def swish_test(x, scale):
     tensor = tf.convert_to_tensor(x)
     swish_result = tensor * tf.sigmoid(tensor * scale)
     return swish_result.numpy()
+
 
 def calc_expect_func(x, y, scale=1.0):
     res = swish_test(x["value"], scale)
