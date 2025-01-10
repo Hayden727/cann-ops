8da28864904526921d142f7d3756d50a0790ebdd
Bellyboom <chen.x@mail.nwpu.edu.cn>
2025-01-10 10:32:29 +0000

    update src/math/addcdiv/tests/st/test_addcdiv.py.
    Signed-off-by: Bellyboom <chen.x@mail.nwpu.edu.cn>

--- src/math/addcdiv/tests/st/test_addcdiv.py
+++ src/math/addcdiv/tests/st/test_addcdiv.py
@@ -5,23 +5,13 @@ import tensorflow as tf
 import numpy as np
 
 
-def addcdiv_test(input_data, x1, x2, value):
+def addcmul_test(input_data, x1, x2, value):
     input_data_tensor = tf.convert_to_tensor(input_data)
     x1_tensor = tf.convert_to_tensor(x1)
     x2_tensor = tf.convert_to_tensor(x2)
     value_tensor = tf.convert_to_tensor(value)
-    ori_dtype = input_data_tensor.dtype
-    if ori_dtype == tf.int32:
-        compute_dtype = tf.int32
-    else:
-        compute_dtype = tf.float32
-    input_data_tensor = tf.cast(input_data_tensor, compute_dtype)
-    x1_tensor = tf.cast(x1_tensor, compute_dtype)
-    x2_tensor = tf.cast(x2_tensor, compute_dtype)
-    value_tensor = tf.cast(value_tensor, compute_dtype)
     res = input_data + x1_tensor / x2_tensor * value_tensor
-    result = tf.cast(res, ori_dtype)
-    return result.numpy()
+    return res.numpy()
 
 
 def calc_expect_func(input_data, x1, x2, value, y):
