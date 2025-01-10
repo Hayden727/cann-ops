05936de717e2dcfcbd26131d9987bace1767558e
zhang <1435504330@qq.com>
2025-01-10 12:40:40 +0000

    update src/math/gelu_grad/op_host/gelu_grad.cpp.
    Signed-off-by: zhang <1435504330@qq.com>

--- src/math/gelu_grad/op_host/gelu_grad.cpp
+++ src/math/gelu_grad/op_host/gelu_grad.cpp
@@ -1,4 +1,4 @@
-/* 
+/*
  * Copyright (C) Huawei Technologies Co., Ltd. 2025. All rights reserved.
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -13,133 +13,134 @@
 #include "graph/utils/type_utils.h"
 #include "tiling/platform/platform_ascendc.h"
 
-namespace optiling {
-const uint32_t BLOCK_SIZE = 32;
-const uint32_t BUFFER_NUM = 2;
-
-static ge::graphStatus TilingFunc(gert::TilingContext* context)  //Tiling函数实现
+namespace optiling
 {
-    GeluGradTilingData tiling;
-    uint64_t ubSize;
-    auto ascendcPlatform = platform_ascendc::PlatformAscendC(context->GetPlatformInfo());
-    ascendcPlatform.GetCoreMemSize(platform_ascendc::CoreMemType::UB, ubSize);
-    ubSize -= 8 * 1024;
-    auto coreNum = ascendcPlatform.GetCoreNum();
-    auto socVersion = ascendcPlatform.GetSocVersion();
+    const uint32_t BLOCK_SIZE = 32;
+    const uint32_t BUFFER_NUM = 2;
 
-    if (socVersion != platform_ascendc::SocVersion::ASCEND910B && socVersion != platform_ascendc::SocVersion::ASCEND310B && context->GetInputDesc(0)->GetDataType() == ge::DT_BF16) {
-        return ge::GRAPH_FAILED;
-    }
-
-    uint32_t inputNum = context->GetInputShape(0)->GetStorageShape().GetShapeSize();
-    uint32_t typeLength = 0;
-    ge::TypeUtils::GetDataTypeLength(context->GetInputDesc(0)->GetDataType(), typeLength);
-    uint32_t inputLength = inputNum * typeLength;
-    uint32_t inputBytes = inputLength / inputNum;
-
-    uint32_t ubDataNumber = 10;
-    uint32_t tileBlockNum = (ubSize / BLOCK_SIZE / BUFFER_NUM) / ubDataNumber;
-    uint32_t tileDataNum = (tileBlockNum * BLOCK_SIZE) / inputBytes;
-
-    uint32_t inputLengthAlgin32 = (((inputLength + BLOCK_SIZE - 1) / BLOCK_SIZE) * BLOCK_SIZE);
-    coreNum = (coreNum <  inputLengthAlgin32 / BLOCK_SIZE) ? coreNum : inputLengthAlgin32 / BLOCK_SIZE;
-    coreNum = (coreNum >= 1) ? coreNum : 1;
-    uint32_t everyCoreInputBlockNum = inputLengthAlgin32 / BLOCK_SIZE / coreNum;
-    uint32_t tailBlockNum = (inputLengthAlgin32 / BLOCK_SIZE) % coreNum;
-    
-    uint32_t smallCoreDataNum = everyCoreInputBlockNum * BLOCK_SIZE / inputBytes;
-    uint32_t smallTileNum = everyCoreInputBlockNum / tileBlockNum;
-    uint32_t finalSmallTileNum = (everyCoreInputBlockNum % tileBlockNum) == 0 ? smallTileNum : smallTileNum + 1;
-    uint32_t smallTailDataNum = smallCoreDataNum - (tileDataNum * smallTileNum);
-    smallTailDataNum = smallTailDataNum == 0 ? tileDataNum : smallTailDataNum;
-    
-    everyCoreInputBlockNum += 1;
-    uint32_t bigCoreDataNum = everyCoreInputBlockNum * BLOCK_SIZE / inputBytes;
-    uint32_t bigTileNum = everyCoreInputBlockNum / tileBlockNum;
-    uint32_t finalBigTileNum = (everyCoreInputBlockNum % tileBlockNum) == 0 ? bigTileNum : bigTileNum + 1;
-    uint32_t bigTailDataNum = bigCoreDataNum - tileDataNum * bigTileNum;
-    bigTailDataNum = bigTailDataNum == 0 ? tileDataNum : bigTailDataNum; 
-    
-    uint32_t versionNum=0 ;
-    if(socVersion == platform_ascendc::SocVersion::ASCEND310B)
+    static ge::graphStatus TilingFunc(gert::TilingContext *context) // Tiling函数实现
     {
-        versionNum=1;
+        GeluGradTilingData tiling;
+        uint64_t ubSize;
+        auto ascendcPlatform = platform_ascendc::PlatformAscendC(context->GetPlatformInfo());
+        ascendcPlatform.GetCoreMemSize(platform_ascendc::CoreMemType::UB, ubSize);
+        ubSize -= 8 * 1024;
+        auto coreNum = ascendcPlatform.GetCoreNum();
+        auto socVersion = ascendcPlatform.GetSocVersion();
+
+        if (socVersion != platform_ascendc::SocVersion::ASCEND910B && socVersion != platform_ascendc::SocVersion::ASCEND310B && context->GetInputDesc(0)->GetDataType() == ge::DT_BF16)
+        {
+            return ge::GRAPH_FAILED;
+        }
+
+        uint32_t inputNum = context->GetInputShape(0)->GetStorageShape().GetShapeSize();
+        uint32_t typeLength = 0;
+        ge::TypeUtils::GetDataTypeLength(context->GetInputDesc(0)->GetDataType(), typeLength);
+        uint32_t inputLength = inputNum * typeLength;
+        uint32_t inputBytes = inputLength / inputNum;
+
+        uint32_t ubDataNumber = 10;
+        uint32_t tileBlockNum = (ubSize / BLOCK_SIZE / BUFFER_NUM) / ubDataNumber;
+        uint32_t tileDataNum = (tileBlockNum * BLOCK_SIZE) / inputBytes;
+
+        uint32_t inputLengthAlgin32 = (((inputLength + BLOCK_SIZE - 1) / BLOCK_SIZE) * BLOCK_SIZE);
+        coreNum = (coreNum < inputLengthAlgin32 / BLOCK_SIZE) ? coreNum : inputLengthAlgin32 / BLOCK_SIZE;
+        coreNum = (coreNum >= 1) ? coreNum : 1;
+        uint32_t everyCoreInputBlockNum = inputLengthAlgin32 / BLOCK_SIZE / coreNum;
+        uint32_t tailBlockNum = (inputLengthAlgin32 / BLOCK_SIZE) % coreNum;
+
+        uint32_t smallCoreDataNum = everyCoreInputBlockNum * BLOCK_SIZE / inputBytes;
+        uint32_t smallTileNum = everyCoreInputBlockNum / tileBlockNum;
+        uint32_t finalSmallTileNum = (everyCoreInputBlockNum % tileBlockNum) == 0 ? smallTileNum : smallTileNum + 1;
+        uint32_t smallTailDataNum = smallCoreDataNum - (tileDataNum * smallTileNum);
+        smallTailDataNum = smallTailDataNum == 0 ? tileDataNum : smallTailDataNum;
+
+        everyCoreInputBlockNum += 1;
+        uint32_t bigCoreDataNum = everyCoreInputBlockNum * BLOCK_SIZE / inputBytes;
+        uint32_t bigTileNum = everyCoreInputBlockNum / tileBlockNum;
+        uint32_t finalBigTileNum = (everyCoreInputBlockNum % tileBlockNum) == 0 ? bigTileNum : bigTileNum + 1;
+        uint32_t bigTailDataNum = bigCoreDataNum - tileDataNum * bigTileNum;
+        bigTailDataNum = bigTailDataNum == 0 ? tileDataNum : bigTailDataNum;
+
+        uint32_t versionNum = 0;
+        if (socVersion == platform_ascendc::SocVersion::ASCEND310B)
+        {
+            versionNum = 1;
+        }
+
+        tiling.set_smallCoreDataNum(smallCoreDataNum);
+        // 一个小核数据个数
+        tiling.set_bigCoreDataNum(bigCoreDataNum);
+        // 一个大核数据个数
+        tiling.set_tileDataNum(tileDataNum);
+        // 一次搬运的数据个数
+        tiling.set_smallTailDataNum(smallTailDataNum);
+        // 小核尾块数据个数
+        tiling.set_bigTailDataNum(bigTailDataNum);
+        // 大核尾块数据个数
+        tiling.set_finalSmallTileNum(finalSmallTileNum);
+        // 小核搬运次数
+        tiling.set_finalBigTileNum(finalBigTileNum);
+        // 大核搬运次数
+        tiling.set_tailBlockNum(tailBlockNum);
+        // 大核数
+        tiling.set_versionNum(versionNum);
+
+        context->SetBlockDim(coreNum);
+        tiling.SaveToBuffer(context->GetRawTilingData()->GetData(), context->GetRawTilingData()->GetCapacity());
+        context->GetRawTilingData()->SetDataSize(tiling.GetDataSize());
+        size_t *currentWorkspace = context->GetWorkspaceSizes(1);
+        currentWorkspace[0] = 0;
+        return ge::GRAPH_SUCCESS;
     }
-    
-    tiling.set_smallCoreDataNum(smallCoreDataNum);
-    //一个小核数据个数
-    tiling.set_bigCoreDataNum(bigCoreDataNum);
-    //一个大核数据个数
-    tiling.set_tileDataNum(tileDataNum);
-    //一次搬运的数据个数
-    tiling.set_smallTailDataNum(smallTailDataNum);
-    //小核尾块数据个数
-    tiling.set_bigTailDataNum(bigTailDataNum);
-    //大核尾块数据个数
-    tiling.set_finalSmallTileNum(finalSmallTileNum);
-    //小核搬运次数
-    tiling.set_finalBigTileNum(finalBigTileNum);
-    //大核搬运次数
-    tiling.set_tailBlockNum(tailBlockNum);
-    //大核数
-    tiling.set_versionNum(versionNum);
-    
-    context->SetBlockDim(coreNum);
-    tiling.SaveToBuffer(context->GetRawTilingData()->GetData(), context->GetRawTilingData()->GetCapacity());
-    context->GetRawTilingData()->SetDataSize(tiling.GetDataSize());
-    size_t *currentWorkspace = context->GetWorkspaceSizes(1);
-    currentWorkspace[0] = 0;
-    return ge::GRAPH_SUCCESS;
-}
 }
 
-
-namespace ge {
-static ge::graphStatus InferShape(gert::InferShapeContext* context)  //shape推导函数实现
+namespace ge
 {
-    const gert::Shape* x1_shape = context->GetInputShape(0);
-    gert::Shape* y_shape = context->GetOutputShape(0);
-    *y_shape = *x1_shape;
-    return GRAPH_SUCCESS;
-}
-}
-
-
-
-namespace ops {
-class GeluGrad : public OpDef {
-public:
-    explicit GeluGrad(const char* name) : OpDef(name)
+    static ge::graphStatus InferShape(gert::InferShapeContext *context) // shape推导函数实现
     {
-        this->Input("dy")
-            .ParamType(REQUIRED)
-            .DataType({ge::DT_FLOAT, ge::DT_FLOAT16, ge::DT_BF16})
-            .Format({ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND})
-            .UnknownShapeFormat({ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND});
-        this->Input("x")
-            .ParamType(REQUIRED)
-            .DataType({ge::DT_FLOAT, ge::DT_FLOAT16, ge::DT_BF16})
-            .Format({ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND})
-            .UnknownShapeFormat({ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND});
-        this->Input("y")
-            .ParamType(REQUIRED)
-            .DataType({ge::DT_FLOAT, ge::DT_FLOAT16, ge::DT_BF16})
-            .Format({ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND})
-            .UnknownShapeFormat({ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND});
-        this->Output("z")
-            .ParamType(REQUIRED)
-            .DataType({ge::DT_FLOAT, ge::DT_FLOAT16, ge::DT_BF16})
-            .Format({ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND})
-            .UnknownShapeFormat({ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND});
-
-        this->SetInferShape(ge::InferShape);
-
-        this->AICore()
-            .SetTiling(optiling::TilingFunc);
-        this->AICore().AddConfig("ascend910b").AddConfig("ascend310b");
-
+        const gert::Shape *x1_shape = context->GetInputShape(0);
+        gert::Shape *y_shape = context->GetOutputShape(0);
+        *y_shape = *x1_shape;
+        return GRAPH_SUCCESS;
     }
-};
+}
 
-OP_ADD(GeluGrad);
+namespace ops
+{
+    class GeluGrad : public OpDef
+    {
+    public:
+        explicit GeluGrad(const char *name) : OpDef(name)
+        {
+            this->Input("dy")
+                .ParamType(REQUIRED)
+                .DataType({ge::DT_FLOAT, ge::DT_FLOAT16, ge::DT_BF16})
+                .Format({ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND})
+                .UnknownShapeFormat({ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND});
+            this->Input("x")
+                .ParamType(REQUIRED)
+                .DataType({ge::DT_FLOAT, ge::DT_FLOAT16, ge::DT_BF16})
+                .Format({ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND})
+                .UnknownShapeFormat({ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND});
+            this->Input("y")
+                .ParamType(REQUIRED)
+                .DataType({ge::DT_FLOAT, ge::DT_FLOAT16, ge::DT_BF16})
+                .Format({ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND})
+                .UnknownShapeFormat({ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND});
+            this->Output("z")
+                .ParamType(REQUIRED)
+                .DataType({ge::DT_FLOAT, ge::DT_FLOAT16, ge::DT_BF16})
+                .Format({ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND})
+                .UnknownShapeFormat({ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND});
+
+            this->SetInferShape(ge::InferShape);
+
+            this->AICore()
+                .SetTiling(optiling::TilingFunc);
+            this->AICore().AddConfig("ascend910b").AddConfig("ascend310b");
+        }
+    };
+
+    OP_ADD(GeluGrad);
 }
