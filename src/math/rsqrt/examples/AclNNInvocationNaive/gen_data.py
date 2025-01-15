c864f77c14b250e4e6b54196cf5e1d8c908661aa
Nice_try <1849404617@qq.com>
2025-01-15 11:57:23 +0000

    update src/math/rsqrt/examples/AclNNInvocationNaive/gen_data.py.
    Signed-off-by: Nice_try <1849404617@qq.com>

--- src/math/rsqrt/examples/AclNNInvocationNaive/gen_data.py
+++ src/math/rsqrt/examples/AclNNInvocationNaive/gen_data.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 # -*- coding:utf-8 -*-
 # Copyright 2022-2023 Huawei Technologies Co., Ltd
-import numpy
 import os
+import numpy as np
 
 def gen_golden_data_simple():
-    dtype = numpy.float16
+    dtype = np.float16
     output_shape = [100, 100]
-    input_x = numpy.random.uniform(1, 4, output_shape).astype(dtype)
+    input_x = np.random.uniform(1, 4, output_shape).astype(dtype)
 
-    golden = (1.0 / numpy.sqrt(input_x)).astype(dtype)
+    golden = (1.0 / np.sqrt(input_x)).astype(dtype)
 
     os.system("mkdir -p input")
     os.system("mkdir -p output")
