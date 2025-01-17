e9ea0e23c47bb7173e3937c1404af179e27eed0f
Nice_try <1849404617@qq.com>
2025-01-17 07:43:28 +0000

    update src/math/rsqrt/tests/st/test_rsqrt.py.
    Signed-off-by: Nice_try <1849404617@qq.com>

--- src/math/rsqrt/tests/st/test_rsqrt.py
+++ src/math/rsqrt/tests/st/test_rsqrt.py
@@ -1,10 +1,15 @@
+#!/usr/bin/python3
+# coding=utf-8
+# Copyright 2025 Huawei Technologies Co., Ltd
 import tensorflow as tf
 import numpy as np
+
 
 def rsqrt_test(x):
     tensor = tf.convert_to_tensor(x)
     result = tf.math.rsqrt(tensor)
     return result.numpy()
+
 
 def calc_expect_func(x, y):
     """
