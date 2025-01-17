1460ea688f8a38607350fe385f9b1c0ff9f66b52
Nice_try <1849404617@qq.com>
2025-01-17 08:12:14 +0000

    update src/math/addcmul/op_kernel/addcmul.cpp.
    Signed-off-by: Nice_try <1849404617@qq.com>

--- src/math/addcmul/op_kernel/addcmul.cpp
+++ src/math/addcmul/op_kernel/addcmul.cpp
@@ -285,3 +285,13 @@ extern "C" __global__ __aicore__ void addcmul(GM_ADDR input_data, GM_ADDR x1, GM
         op.Process();
     }  
 }
+
+#ifndef ASCENDC_CPU_DEBUG
+// call of kernel function
+void addcmul_do(uint32_t blockDim, void *l2ctrl, void *stream,
+                   uint8_t *input_data, uint8_t *x1, uint8_t *x2, uint8_t *value, uint8_t *y,
+                   uint8_t *workspace, uint8_t *tiling)
+{
+    addcmul<<<blockDim, l2ctrl, stream>>>(input_data, x1, x2, value, y, workspace, tiling);
+}
+#endif
