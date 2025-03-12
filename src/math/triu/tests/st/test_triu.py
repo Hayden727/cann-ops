28de53c379cf3e97fc3d706c72b84a49227fd2d6
enkilee <jeffrey0122@163.com>
2025-03-12 09:33:51 +0800

    fix st test file

--- src/math/triu/tests/st/test_triu.py
+++ src/math/triu/tests/st/test_triu.py
@@ -16,7 +16,7 @@ import numpy as np
 
 def triu_test(x, diagonal):
     tensor = tf.convert_to_tensor(x)
-    triu_result = tf.linalg.band_part(tensor, 0, diagonal)
+    triu_result = tf.linalg.band_part(tensor, diagonal, -1)
     return triu_result.numpy()
 
 
