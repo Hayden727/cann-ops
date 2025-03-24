8d6b5a883293c488557c3864dce0e98e9b9528c3
mlxb <malixin6@h-partners.com>
2025-03-24 02:17:03 +0000

    上传代码
    Signed-off-by: mlxb <malixin6@h-partners.com>

---
+++ src/math/inplace_attn_softmax/op_kernel/inplace_attn_softmax.h
@@ -0,0 +1,163 @@
+/**
+ * Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved.
+ * This file is a part of the CANN Open Software.
+ * Licensed under CANN Open Software License Agreement Version 1.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+/**
+ * @file inplace_attn_softmax.h
+ */
+ 
+#ifndef INPLACE_ATTN_SOFTMAX_LARGE_SHAPE_H
+#define INPLACE_ATTN_SOFTMAX_LARGE_SHAPE_H
+
+#include <typeinfo>
+#include "kernel_operator.h"
+#include "inplace_attn_softmax_bash.h"
+
+namespace InplaceAttnSoftmaxOpt {
+using namespace AscendC;
+
+template <typename inType, typename outType, bool isCast>
+class InplaceAttnSoftmax : public InplaceAttnSoftmaxBase<inType, outType, isCast> {
+public:
+    __aicore__ inline InplaceAttnSoftmax(TPipe *pipe)
+    {
+        this->pPipe = pipe;
+    }
+
+    __aicore__ inline void Init(GM_ADDR input_gm, GM_ADDR workspace, const InplaceAttnSoftmaxTilingData *__restrict tilingData)
+    {
+        this->ParseTilingData(tilingData);
+        this->softmaxTilingData_ = tilingData->softmaxTilingData;
+        InitParams();
+        InitAndSetBuffer(input_gm, workspace);
+    }
+
+    __aicore__ inline void Process()
+    {
+        ProcessCoreMultiUbMulti();
+    }
+
+private:
+    __aicore__ inline void InitParams()
+    {
+        this->colLen = this->tilingData_.colLen;
+        this->basicColLen = this->tilingData_.basicColLen;
+
+        this->coreIdx = static_cast<uint32_t>(GetBlockIdx());
+        this->headCoreNum = this->tilingData_.headCoreNum;
+
+        if (this->coreIdx < this->headCoreNum) {
+            this->rowLenPerCore = this->tilingData_.rowLenPerHeadCore;
+            this->basicRowLen = this->tilingData_.basicRowLenHeadCore;
+            this->rowLoop = this->CeilDiv(this->rowLenPerCore, this->basicRowLen);
+            this->baseRow = this->coreIdx * this->rowLenPerCore;
+        } else if (this->coreIdx >= this->headCoreNum && this->coreIdx < this->tilingData_.realCoreNum) {
+            this->rowLenPerCore = this->tilingData_.rowLenPerTailCore;
+            this->basicRowLen = this->tilingData_.basicRowLenTailCore;
+            this->rowLoop = this->CeilDiv(this->rowLenPerCore, this->basicRowLen);
+            this->baseRow = this->headCoreNum * this->tilingData_.rowLenPerHeadCore + (this->coreIdx - this->headCoreNum) * this->rowLenPerCore;
+        } 
+
+        uint32_t alignedNum = BLOCK_SIZE / sizeof(inType);
+        this->sizeHalfLen = this->AlignUp(this->basicColLen, alignedNum);
+        // 若basicColLen比32B还小 -> this->sizeHalfLen == 0 -> sizeHalfLen直接按32B字节算
+        this->tileLength = this->basicRowLen * (this->sizeHalfLen == 0 ? (BLOCK_SIZE / sizeof(inType)) : this->sizeHalfLen);
+        this->rightPadding = this->sizeHalfLen - this->basicColLen;
+    }
+
+    __aicore__ inline void InitAndSetBuffer(GM_ADDR input_gm,GM_ADDR workspace_gm)
+    {
+        // gm数据
+        xGm.SetGlobalBuffer((__gm__ inType *)input_gm,this->tilingData_.rowLen * this->tilingData_.colLen);
+        this->pPipe->InitBuffer(inQueueA, BUFFER_NUM, this->tileLength * sizeof(inType));
+        // 若tilingKey为101, 201，则需要给精度转换留空间
+        if constexpr(isCast) {
+            this->pPipe->InitBuffer(sharedBTempBuf, this->tileLength * sizeof(float));
+            tmpCLocal = sharedBTempBuf.Get<float>();
+        }
+    }
+
+    __aicore__ inline void ProcessCoreMultiUbMulti()
+    {
+        for (uint32_t ridx = 0; ridx < this->rowLoop; ridx++) {
+            this->basicRowLenCal =
+                static_cast<uint32_t>((ridx == this->rowLoop - 1) ? (this->rowLenPerCore - (this->rowLoop - 1) * this->basicRowLen)
+                                                            : this->basicRowLen);  // 每核处理的最后一个行循环单独处理
+            ProcessCoreMultiUbMultiAlign(ridx);
+        }
+    }
+
+    __aicore__ inline void ComputeVecInGmOffset(uint32_t ridx)
+    {
+        if (this->coreIdx < this->headCoreNum) {
+            this->offsetParam.tmpVecGmOffset = this->coreIdx * this->rowLenPerCore * this->colLen + ridx * this->basicRowLen * this->basicColLen;
+        } else {
+            this->offsetParam.tmpVecGmOffset = this->headCoreNum * this->tilingData_.rowLenPerHeadCore * this->colLen +
+                                         (this->coreIdx - this->headCoreNum) * this->rowLenPerCore * this->colLen +
+                                         ridx * this->basicRowLen * this->basicColLen;
+        }
+    }
+
+    __aicore__ inline void ProcessCoreMultiUbMultiAlign(uint32_t ridx)
+    {
+        DataCopyParams splitCopyinParams;
+        DataCopyParams splitCopyoutParams;
+
+        splitCopyinParams = {this->basicRowLenCal,(uint16_t)(this->basicColLen * sizeof(inType)),
+                                0,0};
+        splitCopyoutParams = {this->basicRowLenCal,(uint16_t)(this->basicColLen * sizeof(outType)),0,0};
+        ComputeVecInGmOffset(ridx);
+        CopyIn(this->offsetParam, splitCopyinParams,ridx);
+        PipeBarrier<PIPE_ALL>();
+        Compute(ridx);
+        PipeBarrier<PIPE_ALL>();
+        CopyOut(this->offsetParam, splitCopyoutParams,ridx);
+        PipeBarrier<PIPE_ALL>();
+    }
+
+    __aicore__ inline void CopyIn(InplaceAttnSoftmaxOffsetParam &offsetParam,DataCopyParams &splitCopyinParams,uint32_t ridx)
+    {
+        LocalTensor<inType> aLocal = inQueueA.template AllocTensor<inType>();
+        DataCopyPadParams padParams{true, 0, this->rightPadding, 0};
+        DataCopyPad(aLocal, xGm[this->offsetParam.tmpVecGmOffset], splitCopyinParams, padParams);
+        inQueueA.template EnQue(aLocal);
+    }
+
+    __aicore__ inline void CopyOut(InplaceAttnSoftmaxOffsetParam &offsetParam,DataCopyParams &splitCopyoutParams,uint32_t ridx)
+    {
+        LocalTensor<outType> outLocal = inQueueA.DeQue<outType>(); 
+        DataCopyPad(xGm[this->offsetParam.tmpVecGmOffset], outLocal, splitCopyoutParams);
+        inQueueA.FreeTensor(outLocal);
+    }
+    __aicore__ inline void Compute(uint32_t ridx)
+    {
+        LocalTensor<inType> aLocal = inQueueA.template DeQue<inType>(); 
+        SoftMaxShapeInfo srcShape = { this->basicRowLenCal, this->sizeHalfLen, this->basicRowLenCal, this->basicColLen};
+        if constexpr(isCast) {
+            AscendC::Cast(tmpCLocal, aLocal, AscendC::RoundMode::CAST_NONE, aLocal.GetSize());
+            PipeBarrier<PIPE_V>();
+            SoftMax<float>(tmpCLocal, tmpCLocal, this->softmaxTilingData_, srcShape);
+            PipeBarrier<PIPE_V>();
+            AscendC::Cast(aLocal, tmpCLocal, AscendC::RoundMode::CAST_RINT, aLocal.GetSize());
+            PipeBarrier<PIPE_V>();
+        } else {
+            SoftMax<inType>(aLocal, aLocal, this->softmaxTilingData_, srcShape);
+        }
+        inQueueA.template EnQue<outType>(aLocal);
+    }
+
+private:
+    TQue<QuePosition::VECIN, BUFFER_NUM> inQueueA;
+    GlobalTensor<inType> xGm;
+    TBuf<TPosition::VECCALC> sharedBTempBuf; 
+    // quant
+    LocalTensor<float> tmpCLocal;
+};
+}
+#endif  
---
+++ src/math/inplace_attn_softmax/op_kernel/inplace_attn_softmax_bigshape.h
@@ -0,0 +1,266 @@
+/**
+ * Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved.
+ * This file is a part of the CANN Open Software.
+ * Licensed under CANN Open Software License Agreement Version 1.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+/**
+ * @file inplace_attn_softmax_bigshape.h
+ */
+
+#ifndef INPLACE_ATTN_SOFTMAX_BIG_SHAPE_H
+#define INPLACE_ATTN_SOFTMAX_BIG_SHAPE_H
+
+#include <typeinfo>
+#include "kernel_operator.h"
+#include "inplace_attn_softmax_bash.h"
+
+namespace InplaceAttnSoftmaxOpt {
+using namespace AscendC;
+
+template <typename inType, typename outType, bool isCast>
+class InplaceAttnSoftmaxBigShape : public InplaceAttnSoftmaxBase<inType, outType, isCast> {
+public:
+    __aicore__ inline InplaceAttnSoftmaxBigShape(TPipe *pipe)
+    {
+        this->pPipe = pipe;
+    }
+
+    __aicore__ inline void Init(GM_ADDR x,GM_ADDR workspace, const InplaceAttnSoftmaxTilingData *__restrict tilingData)
+    {
+        this->ParseTilingData(tilingData);
+        InitParams();
+        InitAndSetBuffer(x, workspace);
+    }
+
+    __aicore__ inline void Process()
+    {
+        ProcessCoreMultiUbMultiBigShape();
+    }
+
+private:
+    __aicore__ inline void InitParams()
+    {
+        this->colLen = this->tilingData_.colLen;
+        this->basicColLen = this->tilingData_.basicColLen;
+
+        this->coreIdx = static_cast<uint32_t>(GetBlockIdx());
+        this->headCoreNum = this->tilingData_.headCoreNum;
+
+        if (this->coreIdx < this->headCoreNum) {
+            this->rowLenPerCore = this->tilingData_.rowLenPerHeadCore;
+            this->basicRowLen = this->tilingData_.basicRowLenHeadCore;
+            this->rowLoop = this->CeilDiv(this->rowLenPerCore, this->basicRowLen);
+            this->colLoop = this->CeilDiv(this->colLen, this->basicColLen);
+            this->lastcolLen = this->Ceilabs(this->colLen, this->basicColLen);
+            this->rightPadding = this->basicColLen - this->lastcolLen;
+        } else if (this->coreIdx >= this->headCoreNum && this->coreIdx < this->tilingData_.realCoreNum) {
+            this->rowLenPerCore = this->tilingData_.rowLenPerTailCore;
+            this->basicRowLen = this->tilingData_.basicRowLenTailCore;
+            this->rowLoop = this->CeilDiv(this->rowLenPerCore, this->basicRowLen);
+            this->colLoop = this->CeilDiv(this->colLen, this->basicColLen);
+            this->lastcolLen = this->Ceilabs(this->colLen, this->basicColLen);
+            this->rightPadding = this->basicColLen - this->lastcolLen;
+        } 
+
+        uint32_t alignedNum = BLOCK_SIZE / sizeof(inType);
+    }
+
+    __aicore__ inline void InitAndSetBuffer(GM_ADDR x, GM_ADDR workspace_gm)
+    {
+        // gm数据
+        xGm.SetGlobalBuffer((__gm__ inType *)x,this->tilingData_.rowLen * this->tilingData_.colLen);
+        // queue
+        this->pPipe->InitBuffer(inQueueA, BUFFER_NUM, this->basicColLen * sizeof(inType));
+        this->pPipe->InitBuffer(sharedTempBuf, this->basicColLen * sizeof(inType));
+        tmpALocal = sharedTempBuf.Get<float>(this->basicColLen);
+        if constexpr(isCast) {
+            this->pPipe->InitBuffer(sharedBTempBuf, this->basicColLen * sizeof(float));
+            tmpCLocal = sharedBTempBuf.Get<float>(this->basicColLen);
+        } 
+    }
+
+    __aicore__ inline void ProcessCoreMultiUbMultiBigShape()
+    {
+        uint32_t offsetRow = 0;
+        DataCopyParams splitCopyinParams;
+        DataCopyParams splitCopyoutParams;
+
+        splitCopyinParams = {1,(uint16_t)(this->basicColLen * sizeof(inType)),0,0};
+        splitCopyoutParams = {1,(uint16_t)(this->basicColLen * sizeof(outType)),0,0};
+
+        for (uint32_t ridx = 0; ridx < this->rowLoop; ridx++) {
+            // 每个核心每次循环的起始偏移地址
+            for(uint32_t cidx = 0; cidx < this->colLoop; cidx++){
+                ComputeVecInGmOffset(ridx,cidx);
+                maxCopyIn(this->offsetParam,splitCopyinParams,ridx,cidx);
+            }
+            sumperrow = 0;
+            for(uint32_t cidx = 0; cidx < this->colLoop; cidx++){
+                ComputeVecInGmOffset(ridx,cidx);
+                subCopyIn(this->offsetParam,splitCopyinParams,ridx,cidx);
+            }
+            for(uint32_t cidx = 0; cidx < this->colLoop; cidx++){
+                ComputeVecInGmOffset(ridx,cidx);
+                mulCopyIn(this->offsetParam,splitCopyinParams,ridx,cidx);
+            }
+        }
+    }
+
+    __aicore__ inline void getSplitCopyinParams(uint32_t cidx, DataCopyParams &splitCopyinParams) 
+    {
+        if(cidx == this->colLoop - 1 ){
+            splitCopyinParams = {1,(uint16_t)(this->lastcolLen * sizeof(inType)),0,0};
+        }else{
+            splitCopyinParams = {1,(uint16_t)(this->basicColLen * sizeof(inType)),0,0};
+        }
+    }
+
+    __aicore__ inline void maxCopyIn(InplaceAttnSoftmaxOffsetParam &offsetParam,DataCopyParams &splitCopyinParams,uint32_t ridx,uint32_t cidx)
+    {
+        LocalTensor<inType> aLocal = inQueueA.template AllocTensor<inType>();
+        getSplitCopyinParams(cidx, splitCopyinParams);
+        padParams = {true, 0, 0, 0};
+        DataCopyPad(aLocal, xGm[offsetParam.tmpVecGmOffset], splitCopyinParams, padParams);
+        PipeBarrier<PIPE_ALL>();
+        if constexpr(isCast) {
+            AscendC::Cast(tmpCLocal, aLocal, AscendC::RoundMode::CAST_NONE, aLocal.GetSize());
+            PipeBarrier<PIPE_V>();
+            if(cidx == this->colLoop - 1 && this->lastcolLen != 0){
+                ReduceMax(tmpALocal, tmpCLocal, tmpALocal, this->lastcolLen, false);
+                PipeBarrier<PIPE_V>(); 
+            }else {
+                ReduceMax(tmpALocal, tmpCLocal, tmpALocal, this->basicColLen, false);
+                PipeBarrier<PIPE_V>();
+                }
+        } else
+        {
+            if(cidx == this->colLoop - 1 && this->lastcolLen != 0){
+                ReduceMax(tmpALocal, aLocal, tmpALocal, this->lastcolLen, false);
+                PipeBarrier<PIPE_V>(); 
+            } else {
+                ReduceMax(tmpALocal, aLocal, tmpALocal, this->basicColLen, false);
+                PipeBarrier<PIPE_V>();
+                }
+        }
+        PipeBarrier<PIPE_V>();
+        if(cidx == 0){
+            maxperrow = tmpALocal.GetValue(0);
+        }else {
+            if(static_cast<float>(tmpALocal.GetValue(0)) > maxperrow){
+                maxperrow = tmpALocal.GetValue(0);
+            } 
+        }
+        PipeBarrier<PIPE_V>();
+        inQueueA.FreeTensor(aLocal);
+    }
+
+    __aicore__ inline void subCopyIn(InplaceAttnSoftmaxOffsetParam &offsetParam, DataCopyParams &splitCopyinParams, 
+                                    uint32_t ridx, uint32_t cidx)
+    {
+        LocalTensor<inType> aLocal = inQueueA.template AllocTensor<inType>();
+        getSplitCopyinParams(cidx, splitCopyinParams);
+        padParams = {true, 0, 0, 0};
+        DataCopyPad(aLocal, xGm[offsetParam.tmpVecGmOffset], splitCopyinParams, padParams);
+        PipeBarrier<PIPE_ALL>();
+        if constexpr(isCast) {
+            AscendC::Cast(tmpCLocal, aLocal, AscendC::RoundMode::CAST_NONE, aLocal.GetSize());
+            PipeBarrier<PIPE_V>();
+            Adds<float>(tmpCLocal, tmpCLocal, static_cast<float>(-1*maxperrow), this->basicColLen);
+            PipeBarrier<PIPE_V>();
+            Exp<float>(tmpCLocal, tmpCLocal, this->basicColLen);
+            PipeBarrier<PIPE_ALL>();
+            if(cidx == this->colLoop - 1 && this->lastcolLen != 0){
+                ReduceSum(tmpALocal, tmpCLocal, tmpALocal, this->lastcolLen);
+                PipeBarrier<PIPE_V>();
+            }else {
+                ReduceSum(tmpALocal, tmpCLocal, tmpALocal, this->basicColLen);
+                PipeBarrier<PIPE_V>(); 
+            }
+            PipeBarrier<PIPE_V>();
+            sumperrow = sumperrow + static_cast<float>(tmpALocal.GetValue(0));
+            PipeBarrier<PIPE_V>();
+            AscendC::Cast(aLocal, tmpCLocal, AscendC::RoundMode::CAST_RINT, aLocal.GetSize());
+            PipeBarrier<PIPE_V>();
+            DataCopyPad(xGm[offsetParam.tmpVecGmOffset], aLocal, splitCopyinParams);
+            PipeBarrier<PIPE_ALL>();
+        } else
+        {
+            Adds<inType>(aLocal, aLocal, static_cast<float>(-1*maxperrow), this->basicColLen);
+            PipeBarrier<PIPE_V>();
+            Exp<inType>(aLocal, aLocal, this->basicColLen);
+            PipeBarrier<PIPE_ALL>();
+            DataCopyPad(xGm[offsetParam.tmpVecGmOffset], aLocal, splitCopyinParams);
+            PipeBarrier<PIPE_ALL>();
+            if(cidx == this->colLoop - 1 && this->lastcolLen != 0){
+                ReduceSum(tmpALocal, aLocal, tmpALocal, this->lastcolLen);
+                PipeBarrier<PIPE_V>();
+            }else {
+                ReduceSum(tmpALocal, aLocal, tmpALocal, this->basicColLen);
+                PipeBarrier<PIPE_V>(); 
+            }
+            PipeBarrier<PIPE_V>();
+            sumperrow = sumperrow + static_cast<float>(tmpALocal.GetValue(0));
+            PipeBarrier<PIPE_V>();
+        }
+        inQueueA.FreeTensor(aLocal);
+    }
+
+    __aicore__ inline void mulCopyIn(InplaceAttnSoftmaxOffsetParam &offsetParam,DataCopyParams &splitCopyinParams,uint32_t ridx,uint32_t cidx)
+    {
+        LocalTensor<inType> aLocal = inQueueA.template AllocTensor<inType>();
+        getSplitCopyinParams(cidx, splitCopyinParams);
+        padParams = {true, 0, 0, 0};
+        DataCopyPad(aLocal, xGm[offsetParam.tmpVecGmOffset], splitCopyinParams, padParams);
+        PipeBarrier<PIPE_ALL>();
+        if constexpr(isCast) {
+            AscendC::Cast(tmpCLocal, aLocal, AscendC::RoundMode::CAST_NONE, aLocal.GetSize());
+            PipeBarrier<PIPE_V>();
+            Muls<float>(tmpCLocal, tmpCLocal, static_cast<float>(1 / sumperrow), this->basicColLen);
+            PipeBarrier<PIPE_ALL>();
+            AscendC::Cast(aLocal, tmpCLocal, AscendC::RoundMode::CAST_RINT, aLocal.GetSize());
+            PipeBarrier<PIPE_V>();
+        } else 
+        {
+            Muls<inType>(aLocal, aLocal, static_cast<float>(1 / sumperrow), this->basicColLen);
+            PipeBarrier<PIPE_ALL>();
+        }
+        if(cidx == this->colLoop - 1 && this->lastcolLen != 0){
+            DataCopyPad(xGm[offsetParam.tmpVecGmOffset], aLocal, {1,(uint16_t)(this->lastcolLen * sizeof(inType)),0,0});
+            PipeBarrier<PIPE_ALL>();
+        }else {
+            DataCopyPad(xGm[offsetParam.tmpVecGmOffset], aLocal, {1,(uint16_t)(this->basicColLen * sizeof(inType)),0,0});
+            PipeBarrier<PIPE_ALL>();
+        }
+        inQueueA.FreeTensor(aLocal);
+    }
+
+    __aicore__ inline void ComputeVecInGmOffset(uint32_t ridx,uint32_t cidx)
+    {
+        if (this->coreIdx < this->headCoreNum) {
+            this->offsetParam.tmpVecGmOffset = this->coreIdx * this->rowLenPerCore * this->colLen + ridx * this->colLen + cidx * this->basicColLen;
+        } else {
+            this->offsetParam.tmpVecGmOffset = this->headCoreNum * this->tilingData_.rowLenPerHeadCore * this->colLen +
+                                         (this->coreIdx - this->headCoreNum) * this->rowLenPerCore * this->colLen +
+                                         + ridx * this->colLen + cidx * this->basicColLen;
+        }
+    }
+
+private:
+    TQue<QuePosition::VECIN, BUFFER_NUM> inQueueA;
+    TBuf<TPosition::VECCALC> sharedTempBuf; 
+    TBuf<TPosition::VECCALC> sharedBTempBuf;
+    LocalTensor<float> tmpCLocal;
+    LocalTensor<float> tmpALocal;
+    GlobalTensor<inType> xGm;
+    float maxperrow = 0;
+    float sumperrow;
+    DataCopyPadParams padParams;
+};
+}
+
+#endif  
