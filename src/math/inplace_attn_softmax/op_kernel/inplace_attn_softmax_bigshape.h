c98820779defc5a55df6de71fbd2ee09788e1009
mlxb <malixin6@h-partners.com>
2025-03-25 11:39:14 +0000

    注释PIPE_V
    Signed-off-by: mlxb <malixin6@h-partners.com>

--- src/math/inplace_attn_softmax/op_kernel/inplace_attn_softmax_bigshape.h
+++ src/math/inplace_attn_softmax/op_kernel/inplace_attn_softmax_bigshape.h
@@ -103,25 +103,25 @@ private:
         // PipeBarrier<PIPE_ALL>();
         if constexpr(isCast) {
             AscendC::Cast(tmpCLocal, aLocal, AscendC::RoundMode::CAST_NONE, aLocal.GetSize());
-            PipeBarrier<PIPE_V>();
+            // PipeBarrier<PIPE_V>();
             if(cidx == this->colLoop - 1 && this->lastcolLen != 0){
                 ReduceMax(tmpALocal, tmpCLocal, tmpALocal, this->lastcolLen, false);
-                PipeBarrier<PIPE_V>(); 
+                // PipeBarrier<PIPE_V>(); 
             }else {
                 ReduceMax(tmpALocal, tmpCLocal, tmpALocal, this->basicColLen, false);
-                PipeBarrier<PIPE_V>();
+                // PipeBarrier<PIPE_V>();
                 }
         } else
         {
             if(cidx == this->colLoop - 1 && this->lastcolLen != 0){
                 ReduceMax(tmpALocal, aLocal, tmpALocal, this->lastcolLen, false);
-                PipeBarrier<PIPE_V>(); 
+                // PipeBarrier<PIPE_V>(); 
             } else {
                 ReduceMax(tmpALocal, aLocal, tmpALocal, this->basicColLen, false);
-                PipeBarrier<PIPE_V>();
+                // PipeBarrier<PIPE_V>();
                 }
         }
-        PipeBarrier<PIPE_V>();
+        // PipeBarrier<PIPE_V>();
         if(cidx == 0){
             maxperrow = tmpALocal.GetValue(0);
         }else {
@@ -129,7 +129,7 @@ private:
                 maxperrow = tmpALocal.GetValue(0);
             } 
         }
-        PipeBarrier<PIPE_V>();
+        // PipeBarrier<PIPE_V>();
         inQueueA.FreeTensor(aLocal);
     }
 
@@ -143,43 +143,43 @@ private:
         // PipeBarrier<PIPE_ALL>();
         if constexpr(isCast) {
             AscendC::Cast(tmpCLocal, aLocal, AscendC::RoundMode::CAST_NONE, aLocal.GetSize());
-            PipeBarrier<PIPE_V>();
+            // PipeBarrier<PIPE_V>();
             Adds<float>(tmpCLocal, tmpCLocal, static_cast<float>(-1*maxperrow), this->basicColLen);
-            PipeBarrier<PIPE_V>();
+            // PipeBarrier<PIPE_V>();
             Exp<float>(tmpCLocal, tmpCLocal, this->basicColLen);
             // PipeBarrier<PIPE_ALL>();
             if(cidx == this->colLoop - 1 && this->lastcolLen != 0){
                 ReduceSum(tmpALocal, tmpCLocal, tmpALocal, this->lastcolLen);
-                PipeBarrier<PIPE_V>();
+                // PipeBarrier<PIPE_V>();
             }else {
                 ReduceSum(tmpALocal, tmpCLocal, tmpALocal, this->basicColLen);
-                PipeBarrier<PIPE_V>(); 
+                // PipeBarrier<PIPE_V>(); 
             }
-            PipeBarrier<PIPE_V>();
+            // PipeBarrier<PIPE_V>();
             sumperrow = sumperrow + static_cast<float>(tmpALocal.GetValue(0));
-            PipeBarrier<PIPE_V>();
+            // PipeBarrier<PIPE_V>();
             AscendC::Cast(aLocal, tmpCLocal, AscendC::RoundMode::CAST_RINT, aLocal.GetSize());
-            PipeBarrier<PIPE_V>();
+            // PipeBarrier<PIPE_V>();
             DataCopyPad(xGm[offsetParam.tmpVecGmOffset], aLocal, splitCopyinParams);
             // PipeBarrier<PIPE_ALL>();
         } else
         {
             Adds<inType>(aLocal, aLocal, static_cast<float>(-1*maxperrow), this->basicColLen);
-            PipeBarrier<PIPE_V>();
+            // PipeBarrier<PIPE_V>();
             Exp<inType>(aLocal, aLocal, this->basicColLen);
             // PipeBarrier<PIPE_ALL>();
             DataCopyPad(xGm[offsetParam.tmpVecGmOffset], aLocal, splitCopyinParams);
             // PipeBarrier<PIPE_ALL>();
             if(cidx == this->colLoop - 1 && this->lastcolLen != 0){
                 ReduceSum(tmpALocal, aLocal, tmpALocal, this->lastcolLen);
-                PipeBarrier<PIPE_V>();
+                // PipeBarrier<PIPE_V>();
             }else {
                 ReduceSum(tmpALocal, aLocal, tmpALocal, this->basicColLen);
-                PipeBarrier<PIPE_V>(); 
+                // PipeBarrier<PIPE_V>(); 
             }
-            PipeBarrier<PIPE_V>();
+            // PipeBarrier<PIPE_V>();
             sumperrow = sumperrow + static_cast<float>(tmpALocal.GetValue(0));
-            PipeBarrier<PIPE_V>();
+            // PipeBarrier<PIPE_V>();
         }
         inQueueA.FreeTensor(aLocal);
     }
@@ -193,11 +193,11 @@ private:
         // PipeBarrier<PIPE_ALL>();
         if constexpr(isCast) {
             AscendC::Cast(tmpCLocal, aLocal, AscendC::RoundMode::CAST_NONE, aLocal.GetSize());
-            PipeBarrier<PIPE_V>();
+            // PipeBarrier<PIPE_V>();
             Muls<float>(tmpCLocal, tmpCLocal, static_cast<float>(1 / sumperrow), this->basicColLen);
             // PipeBarrier<PIPE_ALL>();
             AscendC::Cast(aLocal, tmpCLocal, AscendC::RoundMode::CAST_RINT, aLocal.GetSize());
-            PipeBarrier<PIPE_V>();
+            // PipeBarrier<PIPE_V>();
         } else 
         {
             Muls<inType>(aLocal, aLocal, static_cast<float>(1 / sumperrow), this->basicColLen);
