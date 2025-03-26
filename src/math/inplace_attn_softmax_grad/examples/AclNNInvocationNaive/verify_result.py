c2e2c0b06b377e6394be363a52156675e58fbd33
林守宝 <linshoubao@huawei.com>
2025-03-26 08:15:54 +0000

    update src/math/inplace_attn_softmax_grad/examples/AclNNInvocationNaive/verify_result.py.
    Signed-off-by: 林守宝 <linshoubao@huawei.com>

--- src/math/inplace_attn_softmax_grad/examples/AclNNInvocationNaive/verify_result.py
+++ src/math/inplace_attn_softmax_grad/examples/AclNNInvocationNaive/verify_result.py
@@ -18,7 +18,10 @@ MINIMUM = 10e-10
 def verify_result(real_result, golden):
     dtype = np.float32
     real_result = np.fromfile(real_result, dtype=dtype) # 从bin文件读取实际运算结果
+    print(real_result.shape)
+    print(real_result[:10])
     golden = np.fromfile(golden, dtype=dtype) # 从bin文件读取预期运算结果
+    print(golden.shape[:10])
     result = np.abs(real_result - golden) # 计算运算结果和预期结果偏差
     deno = np.maximum(np.abs(real_result), np.abs(golden))  # 获取最大值并组成新数组
     result_atol = np.less_equal(result, LOSS) # 计算绝对误差
