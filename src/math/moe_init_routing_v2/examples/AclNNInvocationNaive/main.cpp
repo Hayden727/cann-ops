9de47745935f4dd49890f57e34267bad25d8f08c
chenmohua <mhchen11@iflytek.com>
2025-03-26 09:05:53 +0000

    update src/math/moe_init_routing_v2/examples/AclNNInvocationNaive/main.cpp.
    Signed-off-by: chenmohua <mhchen11@iflytek.com>

--- src/math/moe_init_routing_v2/examples/AclNNInvocationNaive/main.cpp
+++ src/math/moe_init_routing_v2/examples/AclNNInvocationNaive/main.cpp
@@ -1,7 +1,17 @@
-#include "acl/acl.h"
-#include "aclnn_moe_init_routing_v2.h"
+/**
+ * Copyright (c) 2023-2024 Huawei Technologies Co., Ltd.
+ * This file is a part of the CANN Open Software.
+ * Licensed under CANN Open Software License Agreement Version 1.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
 #include <iostream>
 #include <vector>
+#include "acl/acl.h"
+#include "aclnn_moe_init_routing_v2.h"
 #define CHECK_RET(cond, return_expr) \
   do {                               \
     if (!(cond)) {                   \
