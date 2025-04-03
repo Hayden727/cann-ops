cdb27d0eab924d2c900c720684b973e1e866043a
enkilee <jeffrey0122@163.com>
2025-04-03 10:32:23 +0800

    add mse_loss

---
+++ src/math/mse_loss/examples/AclNNInvocationNaive/verify_result.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+# Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved.
+# This file is a part of the CANN Open Software.
+# Licensed under CANN Open Software License Agreement Version 1.0 (the "License").
+# Please refer to the License for details. You may not use this file except in compliance with the License.
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+# See LICENSE in the root of the software repository for the full text of the License.
+# ======================================================================================================================
+
+import os
+import sys
+import numpy as np
+
+LOSS = 1e-4
+
+
+def verify_result(real_result, golden):
+    result = np.fromfile(real_result, dtype=np.float32)
+    golden = np.fromfile(golden, dtype=np.float32)
+    for i in range(len(result)):
+        diff = abs(result[i] - golden[i])
+        if (diff > LOSS) and (diff / golden[i] > LOSS):
+            error_message = "output[{}] is {}, expect {}".format(i, result[i], golden[i])
+            print(error_message)
+            return False
+    
+    print("test pass")
+    return True
+
+
+if __name__ == '__main__':
+    verify_result(sys.argv[1], sys.argv[2])
