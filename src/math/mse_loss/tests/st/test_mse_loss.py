c75076e83b8e491d36c194ac9a155f8174bf8f1b
enkilee <jeffrey0122@163.com>
2025-04-11 11:39:39 +0800

    fix

--- src/math/mse_loss/tests/st/test_mse_loss.py
+++ src/math/mse_loss/tests/st/test_mse_loss.py
@@ -9,19 +9,10 @@
 # INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
 # See LICENSE in the root of the software repository for the full text of the License.
 # ======================================================================================================================
-import tensorflow as tf
 import numpy as np
 
 def mse_loss_test(predict, label):
-    predict_dtype = predict.dtype
-    if predict_dtype != np.float32:
-        predict = tf.cast(predict, tf.float32).numpy()
-    label_dtype = label.dtype
-    if label_dtype != np.float32:
-        label = tf.cast(label, tf.float32).numpy()
-    res =  np.mean((predict - label) ** 2)
-    if predict_dtype != np.float32:
-        res = tf.cast(res, predict_dtype).numpy()
+    res = np.mean((predict - label) ** 2)
     return res
 
 
