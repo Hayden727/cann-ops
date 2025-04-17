491bebafda4515510eecd4fb665275605f14638b
enkilee <jeffrey0122@163.com>
2025-04-17 08:39:40 +0800

    fix codeckeck error

--- src/math/mse_loss/op_host/mse_loss.cpp
+++ src/math/mse_loss/op_host/mse_loss.cpp
@@ -18,7 +18,7 @@
  * \file mse_loss.cpp
  * \brief
  */
-#include <string.h>
+#include <cstring>
 #include "mse_loss_tiling.h"
 #include "register/op_def_registry.h"
 #include "graph/utils/type_utils.h"
@@ -54,7 +54,7 @@ static ge::graphStatus TilingFunc(gert::TilingContext* context)
     uint32_t mode = static_cast<int32_t>(0);
     
     if (str_len == strlen(mode1)) {
-        for (int i = 0; i < str_len; i++) {
+        for (size_t i = 0; i < str_len; i++) {
             if (reduction[i] != mode1[i]) {
                 break;
             }
@@ -64,7 +64,7 @@ static ge::graphStatus TilingFunc(gert::TilingContext* context)
         }
     }
     if (str_len == strlen(mode2)) {
-        for (int i = 0; i < str_len; i++) {
+        for (size_t i = 0; i < str_len; i++) {
             if (reduction[i] != mode2[i]) {
                 break;
             }
@@ -74,7 +74,7 @@ static ge::graphStatus TilingFunc(gert::TilingContext* context)
         }
     }
     if (str_len == strlen(mode3)) {
-        for (int i = 0; i < str_len; i++) {
+        for (size_t i = 0; i < str_len; i++) {
             if (reduction[i] != mode3[i]) {
                 break;
             }
@@ -99,15 +99,18 @@ static ge::graphStatus TilingFunc(gert::TilingContext* context)
     context->SetBlockDim(1);
 
     auto block_dim = context->GetBlockDim();
+    if (block_dim == 0) {
+        throw std::runtime_error("Block dimension cannot be zero.");
+    }
+
     uint32_t blockLength = 0;
     uint32_t tileLength = 0;
     uint32_t lasttileLength = 0;
 
     blockLength = totalLengthAligned / block_dim;
     tile_num = blockLength / ALIGN_NUM / ub_block_num;
-
     // 数据切分策略： 由于为单核环境，则将tileLength设置得尽可能大，最后单独处理剩余数据
-    if ((totalLengthAligned / block_dim / ALIGN_NUM) % ub_block_num == 0 || tile_num == 0) {  
+    if (ub_block_num != 0 && ((totalLengthAligned / block_dim / ALIGN_NUM) % ub_block_num == 0 || tile_num == 0)) {  
         if (tile_num == 0) {
             tile_num = 1;
         } 
@@ -135,7 +138,7 @@ static ge::graphStatus TilingFunc(gert::TilingContext* context)
                         context->GetRawTilingData()->GetCapacity());
     context->GetRawTilingData()->SetDataSize(tiling.GetDataSize());
     size_t* currentWorkspace = context->GetWorkspaceSizes(1);
-    currentWorkspace[0] = 0;
+    currentWorkspace[0] = static_cast<size_t>(0);
     return ge::GRAPH_SUCCESS;
 }
 }
