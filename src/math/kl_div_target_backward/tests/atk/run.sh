59ecc5754ba4d1e6a0c4884875ec53421547fb85
章鹏飞 <zhangpengfei99@huawei.com>
2025-04-29 12:27:21 +0000

    atk broadcast test

--- src/math/kl_div_target_backward/tests/atk/run.sh
+++ src/math/kl_div_target_backward/tests/atk/run.sh
@@ -1,2 +1,10 @@
 # atk case -f op_kl_div_target_backward.yaml -p generate_reduce.py
-atk node --backend pyaclnn --devices 0 node --backend cpu task -c result/op_kl_div_target_backward/json/all_op_kl_div_target_backward.json -p /home/z00606978/ascend_ops/atk/aclnn_kl_div_target_backward.py --task accuracy -s 0 -e 200 -mt 100
+# atk node --backend pyaclnn --devices 0 node --backend cpu task -c result/op_kl_div_target_backward/json/all_op_kl_div_target_backward.json -p aclnn_kl_div_target_backward.py --task accuracy -s 0 -e 200 -mt 100
+# atk node --backend pyaclnn --devices 0 node --backend npu --devices 1 task -c result/op_kl_div_target_backward/json/all_op_kl_div_target_backward.json -p aclnn_kl_div_target_backward.py --task performance_device -s 0 -e 200 -mt 100
+# atk node --backend pyaclnn --devices 0 node --backend cpu --is_compare False task -c result/op_kl_div_target_backward/json/all_op_kl_div_target_backward.json -p aclnn_kl_div_target_backward.py --task accuracy_dc -e 703 -rn 50
+
+# broadcast
+# atk case -f op_kl_div_target_backward_broadcast.yaml -p generate_broadcast.py
+atk node --backend pyaclnn --devices 0 node --backend cpu task -c result/op_kl_div_target_backward_broadcast/json/all_op_kl_div_target_backward_broadcast.json -p aclnn_kl_div_target_backward.py --task accuracy -s 0 -e 200 -mt 100
+# atk node --backend pyaclnn --devices 0 node --backend npu --devices 1 task -c result/op_kl_div_target_backward_broadcast/json/all_op_kl_div_target_backward_broadcast.json -p aclnn_kl_div_target_backward.py --task performance_device -s 0 -e 200 -mt 100
+# atk node --backend pyaclnn --devices 0 node --backend cpu --is_compare False task -c result/op_kl_div_target_backward_broadcast/json/all_op_kl_div_target_backward_broadcast.json -p aclnn_kl_div_target_backward.py --task accuracy_dc -e 703 -rn 50
