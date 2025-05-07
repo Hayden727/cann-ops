298deda170f8a563356f62badf6e8268431cec12
enkilee <jeffrey0122@163.com>
2025-05-07 16:42:38 +0800

    fix st error

--- src/math/mse_loss_grad/op_host/mse_loss_grad.cpp
+++ src/math/mse_loss_grad/op_host/mse_loss_grad.cpp
@@ -114,12 +114,8 @@ static ge::graphStatus TilingFunc(gert::TilingContext* context)
     tiling.SaveToBuffer(context->GetRawTilingData()->GetData(),
                         context->GetRawTilingData()->GetCapacity());
     context->GetRawTilingData()->SetDataSize(tiling.GetDataSize());
-    auto ascendcPlatform = platform_ascendc::PlatformAscendC(context->GetPlatformInfo());
-    size_t userWorkspaceSize = 0;
-    size_t systemWorkspaceSize = static_cast<size_t>(ascendcPlatform.GetLibApiWorkSpaceSize());
-    size_t *currentWorkspace = context->GetWorkspaceSizes(1);
-    currentWorkspace[0] = userWorkspaceSize + systemWorkspaceSize;
-
+    size_t* currentWorkspace = context->GetWorkspaceSizes(1);
+    currentWorkspace[0] = 0;
     return ge::GRAPH_SUCCESS;
 }
 }
