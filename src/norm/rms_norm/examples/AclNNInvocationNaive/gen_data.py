c2106e645b93a3b631165a5086972d07502058b6
photoker <baochangjin2@h-partners.com>
2025-05-08 10:43:42 +0800

    修改算子RmsNorm

--- src/norm/rms_norm/examples/AclNNInvocationNaive/gen_data.py
+++ src/norm/rms_norm/examples/AclNNInvocationNaive/gen_data.py
@@ -15,20 +15,13 @@ import numpy as np
 
 
 def gen_golden_data_simple():
-    dtype = np.float16
-    input_shape = [8, 2048]
-    output_shape = [8, 2048]
-
-    x = np.random.uniform(-1, 1, input_shape).astype(dtype)
-    y = np.random.uniform(-1, 1, input_shape).astype(dtype)
-
-    golden = np.add(x, y)
-
-    os.system("mkdir -p input")
-    os.system("mkdir -p output")
-    x.astype(dtype).tofile("./input/input_x.bin")
-    y.astype(dtype).tofile("./input/input_y.bin")
-    golden.tofile("./output/golden.bin")
+    input_x = torch.tensor([0, 1, 2, 3, 4, 5, 6, 7, 0, 1, 2, 3, 4, 5, 6, 7, 0, 1, 2, 3, 4, 5, 6, 7,
+                            0, 1, 2, 3, 4, 5, 6, 7]).reshape(2, 16).type(torch.float32)
+    gamma = torch.tensor([0, 1, 2, 3, 4, 5, 6, 7, 0, 1, 2, 3, 4, 5, 6, 7]).reshape(16).type(torch.float32)
+    epsilon = 1e-6
+    out = input_x * torch.rsqrt(input_x.pow(2).mean(-1, keepdim=True) + epsilon)
+    golden = out * gamma
+    golden.numpy().tofile("./output/golden.bin")
 
 if __name__ == "__main__":
     gen_golden_data_simple()
