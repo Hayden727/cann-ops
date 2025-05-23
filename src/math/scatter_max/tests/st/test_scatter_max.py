73c53ffc39d600ebfc32ad2abd267f498c424088
zhanganjie <1414904389@qq.com>
2025-05-23 22:47:33 +0800

    st测试

--- src/math/scatter_max/tests/st/test_scatter_max.py
+++ src/math/scatter_max/tests/st/test_scatter_max.py
@@ -10,7 +10,7 @@
 # See LICENSE in the root of the software repository for the full text of the License.
 # ======================================================================================================================
 
-import numpy as np
+#import numpy as np
 import tensorflow as tf
 
 
