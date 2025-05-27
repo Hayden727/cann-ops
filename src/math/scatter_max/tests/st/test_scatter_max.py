d6385053ae31608db13abbac225329ffc0fb0d60
zhanganjie <1414904389@qq.com>
2025-05-27 10:52:29 +0800

    修改st

--- src/math/scatter_max/tests/st/test_scatter_max.py
+++ src/math/scatter_max/tests/st/test_scatter_max.py
@@ -11,24 +11,29 @@
 # ======================================================================================================================
 
 import numpy as np
-import tensorflow as tf
+import torch
 
 
 def scatter_max_test(x_np, indices_np, updates_np, use_locking=False):
     # 转换为TensorFlow张量（保持原始数据类型）
-    x_tensor = tf.convert_to_tensor(x_np)
-    indices_tensor = tf.convert_to_tensor(indices_np, dtype=tf.int32)
-    updates_tensor = tf.convert_to_tensor(updates_np)
+    # x_tensor = tf.convert_to_tensor(x_np)
+    # indices_tensor = tf.convert_to_tensor(indices_np, dtype=tf.int32)
+    # updates_tensor = tf.convert_to_tensor(updates_np)
+    x_tensor = torch.from_numpy(x_np)
+    indices_tensor = torch.from_numpy(indices_np).to(torch.int32)
+    updates_tensor = torch.from_numpy(updates_np)
     
-    x_var = tf.Variable(x_tensor)
+    # x_var = tf.Variable(x_tensor)
     
     # 调用ScatterMax操作
-    result = tf.raw_ops.ScatterMax(
-        ref=x_var,
-        indices=indices_tensor,
-        updates=updates_tensor,
-        use_locking=use_locking
-    )
+    # result = tf.raw_ops.ScatterMax(
+    #     ref=x_var,
+    #     indices=indices_tensor,
+    #     updates=updates_tensor,
+    #     use_locking=use_locking
+    # )
+    result = torch.scatter_reduce(x_tensor, dim=0, index=indices_tensor,
+                                src=updates_tensor, reduce="max", include_self=True)
     
     # 返回结果的NumPy数组
     return result.numpy()
