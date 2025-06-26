a08c60f214e4cdf6b7b1c29cadbd9ed43c0489d7
韩闯 <1486220883@qq.com>
2025-06-26 08:27:29 +0000

    修改License

--- src/math/fill/op_host/fill.cpp
+++ src/math/fill/op_host/fill.cpp
@@ -1,12 +1,17 @@
 /**
-* @file fill.cpp
-*
-* Copyright (C) 2024. Huawei Technologies Co., Ltd. All rights reserved.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-*/
+ * Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved.
+ * This file is a part of the CANN Open Software.
+ * Licensed under CANN Open Software License Agreement Version 1.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+/**
+ * @file fill.cpp
+ */
+
 #include <iostream>
 #include "fill_tiling.h"
 #include "register/op_def_registry.h"
