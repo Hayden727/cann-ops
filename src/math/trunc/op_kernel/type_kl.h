50df9c1554b8c20d6c180e138eb8cb294bf2b551
sunyanan <tiejipiaoliu@163.com>
2025-07-08 16:01:58 +0800

    修复编码问题

--- src/math/trunc/op_kernel/type_kl.h
+++ src/math/trunc/op_kernel/type_kl.h
@@ -21,31 +21,13 @@
 /// @param T2 类型 2
 #define IS_TYPE(T1, T2) std::is_same<T1,T2>::value
 
-/// @brief 判断VAR是否为TYPE类型。(可在编译期给出结果)
-/// @param VAR 变量
-/// @param T 类型
-#define IS_TYPE_VAR(VAR, T) _Generic((VAR),\
-    T: true,\
-    default: false\
-)
-
 /// @brief 判断是否为浮点型。(可在编译期给出结果)
 /// @param T 类型
 #define IS_FLOAT(T) (IS_TYPE(T,half)||IS_TYPE(T,float)||IS_TYPE(T,double))
-
-/// @brief 判断是否为浮点型。(可在编译期给出结果)
-/// @param VAR 变量
-#define IS_FLOAT_VAR(VAR) (IS_TYPE_VAR(VAR,half)||IS_TYPE_VAR(VAR,float)||IS_TYPE_VAR(VAR,double))
 
 /// @brief 判断是否为整型。(可在编译期给出结果)
 /// @param T 类型
 #define IS_INTEGER(T) (IS_TYPE(T,int8_t)||IS_TYPE(T,int16_t)||IS_TYPE(T,int32_t)||IS_TYPE(T,int64_t)||\
                     IS_TYPE(T,uint8_t)||IS_TYPE(T,uint16_t)||IS_TYPE(T,uint32_t)||IS_TYPE(T,uint64_t))
-
-/// @brief 判断是否为整型。(可在编译期给出结果)
-/// @param VAR 变量
-#define IS_INTEGER_VAR(VAR) (IS_TYPE_VAR(VAR,int8_t)||IS_TYPE_VAR(VAR,int16_t)||IS_TYPE_VAR(VAR,int32_t)||IS_TYPE_VAR(VAR,int64_t)||\
-                        IS_TYPE_VAR(VAR,uint8_t)||IS_TYPE_VAR(VAR,uint16_t)||IS_TYPE_VAR(VAR,uint32_t)||IS_TYPE_VAR(VAR,uint64_t))
-
 
 #endif// TYPE_KUNLUN_H
