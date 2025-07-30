6f1e7cfc5d7a276b9f104e81c593fb4c03d8b759
叶晓辉 <1906152382@qq.com>
2025-07-30 10:32:23 +0800

    0730

--- src/math/reciprocal/examples/AclNNInvocationNaive/main.cpp
+++ src/math/reciprocal/examples/AclNNInvocationNaive/main.cpp
@@ -128,15 +128,12 @@ int CreateAclTensor(const std::vector<T> &hostData, const std::vector<int64_t> &
                     aclDataType dataType, aclTensor **tensor)
 {
     auto size = GetShapeSize(shape) * sizeof(T);
-    // 调用aclrtMalloc申请device侧内存
     auto ret = aclrtMalloc(deviceAddr, size, ACL_MEM_MALLOC_HUGE_FIRST);
     CHECK_RET(ret == ACL_SUCCESS, LOG_PRINT("aclrtMalloc failed. ERROR: %d\n", ret); return FAILED);
 
-    // 调用aclrtMemcpy将host侧数据拷贝到device侧内存上
     ret = aclrtMemcpy(*deviceAddr, size, hostData.data(), size, ACL_MEMCPY_HOST_TO_DEVICE);
     CHECK_RET(ret == ACL_SUCCESS, LOG_PRINT("aclrtMemcpy failed. ERROR: %d\n", ret); return FAILED);
 
-    // 调用aclCreateTensor接口创建aclTensor
     *tensor = aclCreateTensor(shape.data(), shape.size(), dataType, nullptr, 0, aclFormat::ACL_FORMAT_ND, shape.data(),
                               shape.size(), *deviceAddr);
     return SUCCESS;
@@ -144,14 +141,12 @@ int CreateAclTensor(const std::vector<T> &hostData, const std::vector<int64_t> &
 
 int main(int argc, char **argv)
 {
-    // 1. （固定写法）device/stream初始化, 参考acl对外接口列表
-    // 根据自己的实际device填写deviceId
+   
     int32_t deviceId = 0;
     aclrtStream stream;
     auto ret = Init(deviceId, &stream);
     CHECK_RET(ret == 0, LOG_PRINT("Init acl failed. ERROR: %d\n", ret); return FAILED);
 
-    // 2. 构造输入与输出，需要根据API的接口自定义构造
     std::vector<int64_t> inputXShape = {100, 100};
     std::vector<int64_t> outputYShape = {100, 100};
     void *inputXDeviceAddr = nullptr;
@@ -167,20 +162,16 @@ int main(int argc, char **argv)
     size_t fileSize = 0;
     void ** input1=(void **)(&inputXHostData);
     void ** input2=(void **)(&outputYHostData);
-    //读取数据
+
     ReadFile("../input/input_x.bin", fileSize, *input1, inputXShapeSize_1*dataType);
     INFO_LOG("Set input success");
-    // 创建inputX aclTensor
     ret = CreateAclTensor(inputXHostData, inputXShape, &inputXDeviceAddr, aclDataType::ACL_FLOAT16, &inputX);
     CHECK_RET(ret == ACL_SUCCESS, return FAILED);
-    // 创建outputY aclTensor
     ret = CreateAclTensor(outputYHostData, outputYShape, &outputYDeviceAddr, aclDataType::ACL_FLOAT16, &outputY);
     CHECK_RET(ret == ACL_SUCCESS, return FAILED);
 
-    // 3. 调用CANN自定义算子库API
     uint64_t workspaceSize = 0;
     aclOpExecutor *executor;
-    // 计算workspace大小并申请内存
     ret = aclnnReciprocalGetWorkspaceSize(inputX, outputY, &workspaceSize, &executor);
     CHECK_RET(ret == ACL_SUCCESS, LOG_PRINT("aclnnReciprocalGetWorkspaceSize failed. ERROR: %d\n", ret); return FAILED);
     void *workspaceAddr = nullptr;
@@ -188,30 +179,24 @@ int main(int argc, char **argv)
         ret = aclrtMalloc(&workspaceAddr, workspaceSize, ACL_MEM_MALLOC_HUGE_FIRST);
         CHECK_RET(ret == ACL_SUCCESS, LOG_PRINT("allocate workspace failed. ERROR: %d\n", ret); return FAILED;);
     }
-    // 执行算子
     ret = aclnnReciprocal(workspaceAddr, workspaceSize, executor, stream);
     CHECK_RET(ret == ACL_SUCCESS, LOG_PRINT("aclnnReciprocal failed. ERROR: %d\n", ret); return FAILED);
 
-    // 4. （固定写法）同步等待任务执行结束
     ret = aclrtSynchronizeStream(stream);
     CHECK_RET(ret == ACL_SUCCESS, LOG_PRINT("aclrtSynchronizeStream failed. ERROR: %d\n", ret); return FAILED);
 
-    // 5. 获取输出的值，将device侧内存上的结果拷贝至host侧，需要根据具体API的接口定义修改
     auto size = GetShapeSize(outputYShape);
     std::vector<aclFloat16> resultData(size, 0);
     ret = aclrtMemcpy(resultData.data(), resultData.size() * sizeof(resultData[0]), outputYDeviceAddr,
                       size * sizeof(aclFloat16), ACL_MEMCPY_DEVICE_TO_HOST);
     CHECK_RET(ret == ACL_SUCCESS, LOG_PRINT("copy result from device to host failed. ERROR: %d\n", ret); return FAILED);
     void ** output1=(void **)(&resultData);
-    //写出数据
     WriteFile("../output/output_y.bin", *output1, outputYShapeSize_1*dataType);
     INFO_LOG("Write output success");
 
-    // 6. 释放aclTensor，需要根据具体API的接口定义修改
     aclDestroyTensor(inputX);
     aclDestroyTensor(outputY);
 
-    // 7. 释放device资源，需要根据具体API的接口定义修改
     aclrtFree(inputXDeviceAddr);
     aclrtFree(outputYDeviceAddr);
     if (workspaceSize > 0) {
--- src/math/reciprocal/examples/AclNNInvocationNaive/verify_result.py
+++ src/math/reciprocal/examples/AclNNInvocationNaive/verify_result.py
@@ -2,19 +2,16 @@ import os
 import sys
 import numpy as np
 
-loss = 1e-3 # 容忍偏差，一般fp16要求绝对误差和相对误差均不超过千分之一
+loss = 1e-3 
 minimum = 10e-10
 
 def verify_result(real_result, golden):
-    real_result = np.fromfile(real_result, dtype=np.float16) # 从bin文件读取实际运算结果
-    #print("real_result: %f", real_result)
-    golden = np.fromfile(golden, dtype=np.float16) # 从bin文件读取预期运算结果
-    #print("golden: %f", golden)
-    result = np.abs(real_result - golden) # 计算运算结果和预期结果偏差
-    #print("result: %f", result)
-    deno = np.maximum(np.abs(real_result), np.abs(golden))  # 获取最大值并组成新数组
-    result_atol = np.less_equal(result, loss) # 计算绝对误差
-    result_rtol = np.less_equal(result / np.add(deno, minimum), loss) # 计算相对误差
+    real_result = np.fromfile(real_result, dtype=np.float16) 
+    golden = np.fromfile(golden, dtype=np.float16) 
+    result = np.abs(real_result - golden) 
+    deno = np.maximum(np.abs(real_result), np.abs(golden)) 
+    result_atol = np.less_equal(result, loss) 
+    result_rtol = np.less_equal(result / np.add(deno, minimum), loss) 
     if not result_rtol.all() and not result_atol.all():
         if np.sum(result_rtol == False) > real_result.size * loss and np.sum(result_atol == False) > real_result.size * loss: # 误差超出预期时返回打印错误，返回对比失败
             print("[ERROR] result error")
