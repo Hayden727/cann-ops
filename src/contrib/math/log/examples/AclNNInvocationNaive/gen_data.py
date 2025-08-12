71da894fecbd81c35be418be49cd0246cf848457
少年放肆的梦 <17836157803@163.com>
2025-08-12 14:18:11 +0000

    log

--- src/contrib/math/log/examples/AclNNInvocationNaive/gen_data.py
+++ src/contrib/math/log/examples/AclNNInvocationNaive/gen_data.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
 # -*- coding:utf-8 -*-
+# Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved.
+# This file is a part of the CANN Open Software.
+# Licensed under CANN Open Software License Agreement Version 1.0 (the "License").
+# Please refer to the License for details. You may not use this file except in compliance with the License.
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+# See LICENSE in the root of the software repository for the full text of the License.
+# ======================================================================================================================
+
 import os
-import torch
 import numpy as np
 import math
 
-def logM(x, base: float = -1.0, scale: float = 1.0, shift: float = 0.0):
+
+def log(x, base: float = -1.0, scale: float = 1.0, shift: float = 0.0):
     # 转换为 PyTorch 张量
-
     exponent = x * scale + shift
-
     if base != -1.0:
         return np.log(exponent) / math.log(base)
-        print(f"base,scale,shift: {base,scale,shift}")
-        return
-  
-
-    print(f"exponent: {exponent}")
     return np.log(exponent)
 
 def gen_golden_data_simple():
@@ -25,10 +27,8 @@ def gen_golden_data_simple():
     input_x = np.random.uniform(1, 4, output_shape).astype(dtype)
     
     # 计算 golden 数据
-    golden = logM(input_x, -1.0, 5.0, 5.0).astype(dtype)
+    golden = log(input_x, -1.0, 5.0, 5.0).astype(dtype)
 
-
-    
     os.system("mkdir -p input")
     os.system("mkdir -p output")
 
